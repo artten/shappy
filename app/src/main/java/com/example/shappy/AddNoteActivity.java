dae8d27269a77b68e5d0acb4c1c048258fc54ed3
Artiom Divak <artten@gmail.com>
2020-09-09 20:14:27 +0200

    added an add note_activity

--- app/src/main/java/com/example/shappy/AddNoteActivity.java
+++ app/src/main/java/com/example/shappy/AddNoteActivity.java
@@ -1,4 +1,58 @@
 package com.example.shappy;
 
-public class AddNoteActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddNoteActivity extends AppCompatActivity {
+    public static final String EXTRA_NAME =
+            "com.example.shappy.EXTRA_NAME";
+    public static final String EXTRA_PRICE =
+            "com.example.shappy.EXTRA_PRICE";
+    private EditText editTextName;
+    private EditText editTextPrice;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        editTextName = findViewById(R.id.edit_text_name);
+        editTextPrice = findViewById(R.id.edit_text_price);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Note");
+    }
+    private void saveNote() {
+        String name = editTextName.getText().toString();
+        String price = editTextPrice.getText().toString();
+        if (name.trim().isEmpty() || price.trim().isEmpty()) {
+            Toast.makeText(this, "Please insert a title and description", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent data = new Intent();
+        data.putExtra(EXTRA_NAME, name);
+        data.putExtra(EXTRA_PRICE, price);
+        setResult(RESULT_OK, data);
+        finish();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_note:
+                saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
--- app/src/main/java/com/example/shappy/MainActivity.java
+++ app/src/main/java/com/example/shappy/MainActivity.java
@@ -9,24 +9,41 @@ import androidx.lifecycle.ViewModelStoreOwner;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-private NoteViewModel noteViewModel;
+    public static final int ADD_NOTE_REQUEST = 1;
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FloatingActionButton buttonAddNote = findViewById(R.id.add);
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+                startActivityForResult(intent, ADD_NOTE_REQUEST);
+            }
+        });
+
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
+
         final NoteAdapter adapter = new NoteAdapter();
         recyclerView.setAdapter(adapter);
+
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
@@ -34,5 +51,18 @@ private NoteViewModel noteViewModel;
                 adapter.setNotes(notes);
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK) {
+            String name = data.getStringExtra(AddNoteActivity.EXTRA_NAME);
+            String price = data.getStringExtra(AddNoteActivity.EXTRA_PRICE);
+            Note note = new Note(name ,price);
+            noteViewModel.insert(note);
+            Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Note not saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
